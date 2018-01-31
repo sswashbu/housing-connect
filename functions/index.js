dee287a46982d2b193c31f04628374d24c935ff0
root <root@DESKTOP-VVOVE5A.localdomain>
2018-01-30 16:06:17 -0800

    fixed stuff

--- functions/index.js
+++ functions/index.js
@@ -3,48 +3,20 @@ const firebase = require('firebase-admin');
 const express = require('express');
 const engines = require('consolidate');
 
-const firebaseApp = firebase.initializeApp(
-    functions.config().firebase
-);
-
-function delayedPush(obj, ref) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            ref.push(obj)
-                .then(resolve, reject);
-        }, 1);
-    });
-}
-
-function getFacts() {
-    const ref = firebaseApp.database().ref('facts');
-    return ref.once('value').then(snap => snap.val());
-}
-
-function addFacts() {
-    const ref = firebaseApp.database().ref('facts');
-    delayedPush({"text":"whats up?!"}, ref).then(snap => snap.val())
-}
-
 const app = express();
 app.engine('hbs', engines.handlebars);
-app.set('views', './views');
+app.set('views', '../public');
 app.set('view engine', 'hbs');
 
 app.get('/', (request, response) => {
     response.set('Cache-Control', 'public, max-age=300, s-maxage=600');
-    addFacts()
-        // .then(getFacts())
-        // .then(facts => {
-        //     response.render('index', {facts});
-        // })
+    let text = "Button";
+    response.render('index', { text });
 });
 
-app.get('/facts.json', (request, response) => {
+app.get('/', (request, response) => {
     response.set('Cache-Control', 'public, max-age=300, s-maxage=600');
-    getFacts().then(facts => {
-        response.json(facts);
-    });
+    response.render('index', { });
 });
 
 exports.app = functions.https.onRequest(app);
